8c2c2bb44a8f82728e94b34d64509663d2e00a07
Georgegitahi <petergeorgembugua724@gmail.com>
2024-03-25 22:34:00 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(){
+console.log("This Sturday, I want to roller-skate!")
+}
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(){
+    console.log("This Monday, I will go to the office")
+}
+function mondayWork(activity='go to the office'){
+return 'This Monday, I will go to the office.'
+}
+function mondayWork(activity="work from home"){
+    return "This Monday, I will work from home."
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(string="*") {
+    return function(parameter="special") {
+        return `You are ${string}${parameter}${string}!`
+    }
+
+
+}
